d6ed790974a14c21af7a96bec92518a7ed54c917
Nic Gordon <gordonna@gmail.com>
2016-03-06 22:10:34 +1100

    Test rpi-gpio

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const bed = require('./lib/bed');
-const readline = require('readline');
+// const bed = require('./lib/bed');
+// const readline = require('readline');
 
-const rl = readline.createInterface(process.stdin, process.stdout);
+// const rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.setPrompt('> ');
-rl.prompt();
+// rl.setPrompt('> ');
+// rl.prompt();
 
-rl.on('line', (line) => {
-  bed.updateStatus();
-  rl.prompt();
-}).on('close', () => {
-  process.exit(0);
+// rl.on('line', (line) => {
+//   bed.updateStatus();
+//   rl.prompt();
+// }).on('close', () => {
+//   process.exit(0);
+// });
+
+
+var gpio = require('rpi-gpio');
+
+gpio.on('change', function(channel, value) {
+  console.log('Channel ' + channel + ' value is now ' + value);
 });
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
